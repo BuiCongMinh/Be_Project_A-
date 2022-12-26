edb159ec9a4525fb9290b4ea5df2534cb7b094b2
BuiCongMinh <65665940+BuiCongMinh@users.noreply.github.com>
2022-12-27 00:07:14 +0700

    #30 Su Dung MýSQl voi NodeJS

--- src/app.js
+++ src/app.js
@@ -6,11 +6,28 @@ const app = express()
 const port = process.env.PORT || 3333
 const hostName = process.env.HOST_NAME
 const webRouter = require('./routes/web')
+const mysql = require('mysql2')
 
 // config teamplate engine ! 
 configViewEngine(app)
 
 // khai báo router
 app.use('/',webRouter)
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    port:3307,  //default:3306
+    user: 'root', //default: empty (đăng nhập ko cần password)
+    password:'123456',
+    database:'hoidanit'
+})
+
+connection.query(
+    'SELECT * FROM Users u',
+    function(err, results, fields){
+        console.log(">>>results=",results);
+        console.log(">>> fields=",fields);
+    }
+)
 
 app.listen(port, hostName, () => { console.log(`http://${hostName}:${port}`) }) //chú ý ko được thêm dấu ; 
