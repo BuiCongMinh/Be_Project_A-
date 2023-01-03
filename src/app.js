d10af005886f7983275f333c78d2b384a97963f3
BuiCongMinh <65665940+BuiCongMinh@users.noreply.github.com>
2023-01-03 16:49:18 +0700

    hoan thanh Section 8: Chapter 7: RESTful APIs (với Mongoose/MongoDB)
    hoan thanh Section 8: Chapter 7:RESTful APIs (với Mongoose/MongoDB)

--- src/app.js
+++ src/app.js
@@ -6,9 +6,8 @@ const app = express()
 const port = process.env.PORT || 3333
 const hostName = process.env.HOST_NAME
 const webRouter = require('./routes/web')
+const api = require('./routes/api')
 const connection = require('./config/database')
-
-
 
 // config req.body
 app.use(express.json())
@@ -19,7 +18,7 @@ configViewEngine(app)
 
 // khai báo router
 app.use('/', webRouter);
-
+app.use('/api/',api);        // lưu ý là trước mỗi funtion vo danh phải thêm dấu ; (phải ; ở cuối mỗi câu lệnh trước thì sau đó mới viết được funtion vô danh)
 
 
 
